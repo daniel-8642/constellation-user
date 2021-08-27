5817cc2aedf7292d40f7243fbd54249a983dc654
王海涛 <859926750@qq.com>
2021-08-27 14:14:34 +0800

    错误提示界面美化

--- src/pages/ShowInfo.vue
+++ src/pages/ShowInfo.vue
@@ -31,6 +31,10 @@
         </div>
       </div>
     </div>
+    <a-modal v-model="modalVisible" v-bind:title=errMessage @ok="handleOk">
+      <p>{{ errDescription }}</p>
+      <p>是否返回首页?</p>
+    </a-modal>
   </div>
 </template>
 
@@ -39,9 +43,19 @@ export default {
   name: "showInfo",
   data: function () {
     return {
+      //详细信息框弹出状态
+      modalVisible: false,
+      //错误标题
+      errMessage: "",
+      //错误详细信息
+      errDescription: "",
+      //加载中
       loading: true,
-      loadTimer:"",
-      timeout:false,
+      //加载超时定时器
+      loadTimer: "",
+      //加载超时标志
+      timeout: false,
+      //保存服务返回信息
       name: '',
       QFriend: '',
       color: '',
@@ -57,14 +71,35 @@ export default {
       error_code: 0
     }
   },
-  methods:{
-    checkLoading(){
-      if (this.loading===true){this.timeout=true}
+  methods: {
+    //处理加载api超时
+    checkLoading() {
+      if (this.loading === true) {
+        this.timeout = true
+      }
+    },
+    //弹出错误信息
+    openErrNotification() {
+      this.$notification.open({
+        message: this.errMessage,
+        description: this.errDescription,
+        onClick: () => {
+          this.showModal()
+        },
+      });
+    },
+    //信息详情窗口
+    showModal() {
+      this.modalVisible = true;
+    },
+    handleOk(e) {
+      console.log(e);
+      this.modalVisible = false;
     },
   },
   created() {
     this.loading = true;
-    this.loadTimer=setTimeout(this.checkLoading,8000)
+    this.loadTimer = setTimeout(this.checkLoading, 8000)
     if (this.$store.state.consName !== 'none') {
       this.$axios({
         method: 'get',
@@ -75,27 +110,48 @@ export default {
           consName: this.$store.getters.consName.name,
         }
       }).then((response) => {
-        console.log(response.data)
-        this.name = response.data.name
-        this.QFriend = response.data.QFriend
-        this.color = response.data.color
-        this.datetime = response.data.datetime
-        this.health = response.data.health
-        this.love = response.data.love
-        this.work = response.data.work
-        this.money = response.data.money
-        this.number = response.data.number
-        this.summary = response.data.summary
-        this.all = response.data.all
-        this.result_code = response.data.result_code
-        this.error_code = response.data.error_code
-        this.loading = false
+        //错误处理
+        console.log(response)
+        if (response.data.error_code===205801) {
+          this.errMessage="错误的运势类型"
+          this.errDescription="错误号: 205801"
+          this.openErrNotification()
+        }else if(response.data.error_code===205802) {
+          this.errMessage="错误的星座名称"
+          this.errDescription="错误号: 205802 星座:"+this.$store.getters.consName.name
+          this.openErrNotification()
+        }
+        else if(parseInt(response.data.error_code)<20000&&parseInt(response.data.error_code)>=10000){
+          this.errMessage="系统错误 : "+response.data.error_code
+          this.errDescription="系统错误 : "+response.data.error_code
+          this.openErrNotification()
+        }
+        else if (response.data.error_code===0)
+        {
+          this.name = response.data.name
+          this.QFriend = response.data.QFriend
+          this.color = response.data.color
+          this.datetime = response.data.datetime
+          this.health = response.data.health
+          this.love = response.data.love
+          this.work = response.data.work
+          this.money = response.data.money
+          this.number = response.data.number
+          this.summary = response.data.summary
+          this.all = response.data.all
+          this.result_code = response.data.result_code
+          this.error_code = response.data.error_code
+          this.loading = false
+        }
       }).catch((error) => {
-        console.log(error)//todo:错误处理
+        this.errMessage = "请求异常"
+        this.errDescription = String(error)
+        this.openErrNotification()
       })
     } else {
       this.$router.push('/')
     }
+
   },
   beforeDestroy() {
     clearTimeout(this.loadTimer)
